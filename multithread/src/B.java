1c14a59b2ef64c828f09bdf2c926fe59f23c900a
ganesangs <msdganesan7@gmail.com>
2023-01-10 17:27:21 +0530

    myfirstcommit

---
+++ multithread/src/B.java
@@ -0,0 +1,19 @@
+class B 
+{
+	public static void main(String[] args) 
+	{
+		for (int i = 1; i <= 100; i++)
+		{
+			System.out.println("loop: " + i);
+		}
+        for (int i = 101; i <= 200; i++)
+        {
+			System.out.println("loop2: " + i);
+        }
+        for (int i = 201; i <= 300; i++)
+		{
+			System.out.println("loop3: " + i);
+		}
+     
+	}
+}
